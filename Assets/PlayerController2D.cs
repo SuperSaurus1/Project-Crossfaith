fbcb01ca9de3f9648730f6f587032dd3829d5139
Simeon_Pillsbury <simeon.pillsbury@gmail.com>
2021-04-07 11:08:30 -0400

    trying to save things

--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
-    Animator animator;
+   // Animator animator;
     Rigidbody2D rb2d;
-    SpriteRenderer spriteRenderer;
-    bool isGrounded;
+    //SpriteRenderer spriteRenderer;
+    //bool isGrounded;
+    public float speed;
+    public float jumpForce;
+    Vector3 NewForce;
     
+
     [SerializeField]
-    Transform GroundCheck;
+    //Transform GroundCheck;
 
         
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        //animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        NewForce = new Vector3(horizontal, 1f, 0f);
 
     }
-    
-    private void FixedUpdate()
+
+    void FixedUpdate()
     {
-        int speed = 2;
+      
         //var maxSpeed = currentMaxSpeed;
         //var acc = maxSpeed;
-        if (Physics2D.Linecast(transform.position, GroundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        /*if (Physics2D.Linecast(transform.position, GroundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
         {
          isGrounded = true;
         }
@@ -37,20 +43,21 @@ public class PlayerController2D : MonoBehaviour
             {
               isGrounded = false;
             }
-
-        if (Input.GetKey("d") && rb2d.velocity.x <= 7 || Input.GetKey("right") && rb2d.velocity.x <= 7)
+        */
+       
+        //transform.Translate(Vector2.right * Time.deltaTime * speed * horizontal);
+        if (Input.GetButtonDown("Horizontal"))
+        rb2d.AddForce(NewForce, (ForceMode2D)ForceMode.Acceleration);
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb2d.velocity.y) < .1)
         {
-            if (rb2d.velocity.x <= 10)
-            {
-                rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
-                spriteRenderer.flipX = false;
-                
-            }
+            rb2d.AddRelativeForce(Vector2.up * jumpForce);
         }
-                     //if (Input.GetKey("d") && rb2d.velocity.x == 10)
-                     //{
-                       // rb2d.velocity = new Vector2((float)(rb2d.velocity.x + 0.25), rb2d.velocity.y);
-                     //}
+        /*if (Input.GetKey("d") && rb2d.velocity.x <= 7 || Input.GetKey("right") && rb2d.velocity.x <= 7)
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            spriteRenderer.flipX = false;
+        }
+                    
                 
                
                 else if (Input.GetKey("a") || Input.GetKey("left"))
@@ -58,16 +65,13 @@ public class PlayerController2D : MonoBehaviour
             rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
                     spriteRenderer.flipX = true;
                 }
-
+        
                 else
                 {
                     rb2d.velocity = new Vector2(0, rb2d.velocity.y);
                 }
-                if (Input.GetKey("space") && isGrounded)
-                {
-                    rb2d.velocity = new Vector2(rb2d.velocity.x, 3);
+        */
 
-                }
-            }
-    }
+     }
+}
 
